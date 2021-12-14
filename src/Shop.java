2e91d3e9c70c01030b52d49f94bccd7aeebbb406
DDan05 <dnipper@holyghostprep.org>
2021-12-13 19:23:52 -0500

    Shop is complete

--- src/Shop.java
+++ src/Shop.java
@@ -27,69 +27,48 @@ public class Shop {
             //if player chooses to enter shop, 3 random items and their prices are displayed
             if (userResponse.equals("yes")) {
                 Random r = new Random();
-                int itemNum = (int) (Math.floor(Math.random() * 6) + 1);
+                int itemNum = (int) (Math.floor(Math.random() * 3) + 1);
 
+                //item 1 is health
                 if (itemNum == 1) {
-                    item1 = "healing potion: 20 coins";
-                    coinsLost = 20;
-                } else if (itemNum == 2) {
-                    item1 = "shield: 15 coins";
-                    coinsLost = 15;
-                } else if (itemNum == 3) {
-                    item1 = "protective boots: 5 coins";
-                    coinsLost = 5;
-                } else if (itemNum == 4) {
-                    item1 = "apple: 5 coins";
-                    coinsLost = 5;
-                } else if (itemNum == 5) {
-                    item1 = "magic sword: 30 coins";
-                    coinsLost = 30;
-                } else {
                     item1 = "birthday cake: 10 coins";
                     coinsLost = 10;
+                } else if (itemNum == 2) {
+                    item1 = "apple: 5 coins";
+                    coinsLost = 5;
+                } else {
+                    item1 = "healing potion: 20 coins";
+                    coinsLost = 20;
                 }
 
-                itemNum = (int) (Math.floor(Math.random() * 6) + 1);
+                //item 2 is weapons
+                itemNum = (int) (Math.floor(Math.random() * 3) + 1);
                 if (itemNum == 1) {
-                    item2 = "healing potion: 20 coins";
-                    coinsLost = 20;
-                } else if (itemNum == 2) {
-                    item2 = "shield: 15 coins";
-                    coinsLost = 15;
-                } else if (itemNum == 3) {
-                    item2 = "protective boots: 5 coins";
-                    coinsLost = 5;
-                } else if (itemNum == 4) {
-                    item2 = "apple: 5 coins";
-                    coinsLost = 5;
-                } else if (itemNum == 5) {
                     item2 = "magic sword: 30 coins";
                     coinsLost = 30;
+                } else if (itemNum == 2) {
+                    item2 = "attack potion: 20 coins";
+                    coinsLost = 20;
                 } else {
-                    item2 = "birthday cake: 10 coins";
-                    coinsLost = 10;
+                    item2 = "crossbow: 25 coins";
+                    coinsLost = 25;
                 }
 
-                itemNum = (int) (Math.floor(Math.random() * 6) + 1);
+
+                //item 3 is armor
+                itemNum = (int) (Math.floor(Math.random() * 3) + 1);
                 if (itemNum == 1) {
-                    item3 = "healing potion: 20 coins";
+                    item3 = "shield: 20 coins";
                     coinsLost = 20;
                 } else if (itemNum == 2) {
-                    item3 = "shield: 15 coins";
+                    item3 = "protective boots: 15 coins";
                     coinsLost = 15;
-                } else if (itemNum == 3) {
-                    item3 = "protective boots: 5 coins";
-                    coinsLost = 5;
-                } else if (itemNum == 4) {
-                    item3 = "apple: 5 coins";
-                    coinsLost = 5;
-                } else if (itemNum == 5) {
-                    item3 = "magic sword: 30 coins";
-                    coinsLost = 30;
                 } else {
-                    item3 = "birthday cake: 10 coins";
-                    coinsLost = 10;
+                    item3 = "helmet: 15 coins";
+                    coinsLost = 15;
                 }
+
+
                 System.out.println(" ");
                 System.out.println("Here are today's options:");
                 System.out.println(item1);
@@ -105,10 +84,10 @@ public class Shop {
 
                 if (itemIntChoose == 1) {
                     itemChosen = item1;
-
+                    
                 } else if (itemIntChoose == 2) {
                     itemChosen = item2;
-
+                    
                 } else if (itemIntChoose == 3) {
                     itemChosen = item3;
 
@@ -116,8 +95,11 @@ public class Shop {
                     return;
                 }
                 //the item chosen by the player is added to their inventory
+                //coins are subtracted from the player based on what they bought
+
                 h.setInvItem(itemChosen, 0);
                 int netCoins = h.getCoins() - coinsLost;
+
                 System.out.println("You chose " + itemChosen + ". You have " + netCoins  + " coins remaining.");
                 return;
 
